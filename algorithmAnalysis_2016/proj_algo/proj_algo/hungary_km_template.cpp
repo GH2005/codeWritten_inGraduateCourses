4ca33239faa03cecbf0de4d1a4d6a10c146f3cc3
Yang Wang <pitsou7@gmail.com>
2018-11-23 13:28:03 -0400

    Update hungary_km_template.cpp

--- algorithmAnalysis_2016/proj_algo/proj_algo/hungary_km_template.cpp
+++ algorithmAnalysis_2016/proj_algo/proj_algo/hungary_km_template.cpp
@@ -25,78 +25,78 @@ TYPE d;
 // KM addon
 #ifdef KM
 void InitLabels() {
-	for (int i = 0; i < cleft; i++) {
-		TYPE maxweight = MIN_INFI;
-		for (int j = 0; j < cright; j++) {
-			if (weight[i][j] > maxweight) maxweight = weight[i][j];
-		}
-		leftLabel[i] = maxweight;
-	}
-	memset(rightLabel, 0, sizeof(rightLabel));
+  for (int i = 0; i < cleft; i++) {
+    TYPE maxweight = MIN_INFI;
+    for (int j = 0; j < cright; j++) {
+      if (weight[i][j] > maxweight) maxweight = weight[i][j];
+    }
+    leftLabel[i] = maxweight;
+  }
+  memset(rightLabel, 0, sizeof(rightLabel));
 }
 #endif
 // InitMatch
 void InitMatch() {
-	for (int i = 0; i < cright; i++) maty[i] = -1;
+  for (int i = 0; i < cright; i++) maty[i] = -1;
 }
 // augmenting path searching
 bool DFS(int theLeft) {
-	treeleft[theLeft] = true;
-	for (int i = 0; i < cright; i++) {	// each right node
-		if (treeright[i]) continue;	// only a new right node counts
+  treeleft[theLeft] = true;
+  for (int i = 0; i < cright; i++) {	// each right node
+    if (treeright[i]) continue;	// only a new right node counts
 #ifdef KM
-		TYPE t = leftLabel[theLeft] + rightLabel[i] - weight[theLeft][i];
-		if (t == 0) { // it's a prerequisite to be in the equal subgraph
-			treeright[i] = true;
-			if (maty[i] == -1 || DFS(maty[i])) {
-				maty[i] = theLeft;
-				return true;
-			}
-		}
-		else if (d > t) {
-			d = t;
-		}
+    TYPE t = leftLabel[theLeft] + rightLabel[i] - weight[theLeft][i];
+    if (t == 0) { // it's a prerequisite to be in the equal subgraph
+      treeright[i] = true;
+      if (maty[i] == -1 || DFS(maty[i])) {
+        maty[i] = theLeft;
+        return true;
+      }
+    }
+    else if (d > t) {
+      d = t;
+    }
 #else
-		if (weight[theLeft][i] == MIN_INFI) continue;
-		treeright[i] = true;
-		if (maty[i] == -1 || DFS(maty[i])) {
-			maty[i] = theLeft;
-			return true;
-		}
+    if (weight[theLeft][i] == MIN_INFI) continue;
+    treeright[i] = true;
+    if (maty[i] == -1 || DFS(maty[i])) {
+      maty[i] = theLeft;
+      return true;
+    }
 #endif
-	} // for: each right node
-	// when false is returned, the alternate tree is a full one
-	return false;
+  } // for: each right node
+  // when false is returned, the alternate tree is a full one
+  return false;
 }
 // main routine of the algorithm
 int HKMprocess() { // return count of DFSSucc.
 #ifdef KM
-	InitLabels();
+  InitLabels();
 #endif // KM
-	InitMatch();
-	int csucc_dfs  = 0;
-	for (int i = 0; i < cleft; i++) { // each left node
-		memset(treeleft, 0, sizeof(treeleft));
-		memset(treeright, 0, sizeof(treeright));
+  InitMatch();
+  int csucc_dfs = 0;
+  for (int i = 0; i < cleft; i++) { // each left node
+    memset(treeleft, 0, sizeof(treeleft));
+    memset(treeright, 0, sizeof(treeright));
 #ifdef KM
-		d = MAX_INFI;
-		while (!DFS(i)) {
-			// update labels.
+    d = MAX_INFI;
+    while (!DFS(i)) {
+      // update labels.
       for (int j = 0; j < cleft; j++) {
         if (!treeleft[j]) continue;
         leftLabel[j] -= d;
       }
       for (int k = 0; k < cright; k++) {
-				if (!treeright[k]) continue;
+        if (!treeright[k]) continue;
         rightLabel[k] += d;
       }
-			d = MAX_INFI;
-			memset(treeleft, 0, sizeof(treeleft));
-			memset(treeright, 0, sizeof(treeright));
+      d = MAX_INFI;
+      memset(treeleft, 0, sizeof(treeleft));
+      memset(treeright, 0, sizeof(treeright));
     }
 #else
     if (DFS(i)) csucc_dfs++;
 #endif
-	}
-	return csucc_dfs;
+  }
+  return csucc_dfs;
 }
